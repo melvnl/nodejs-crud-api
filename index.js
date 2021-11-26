77c8dbb3ba0faf3305332fcf8ec5d112e5cfec44
himelv <63998835+melvnl@users.noreply.github.com>
2021-11-26 16:05:48 +0700

    add: make endpoint in index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+
+const db = require('./queries')
+
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
@@ -13,6 +16,12 @@ app.use(
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUsersById)
+app.post('/users', db.createUser)
+app.put('/users', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
